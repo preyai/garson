ec631aa40563f83a7a44857d32f9e23e085d0d76
David Aizman <preyaizman@gmail.com>
2021-08-26 11:19:11 +0300

    rel

---
+++ src/components/other/Releases.js
@@ -0,0 +1,9 @@
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import Calendar from 'react-calendar'
+import Relase from "../other/Relase"
+
+export default function Releases(props) {
+    return (
+        
+    )
+}
